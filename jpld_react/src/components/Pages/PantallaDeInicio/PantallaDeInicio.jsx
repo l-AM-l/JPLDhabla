16b6b7481a756937f821421748857f5504d5e7e0
Rodrigo López <rodlopg@gmail.com>
2025-09-15 12:13:48 -0600

    Added assets directory for images

--- jpld_react/src/components/Pages/PantallaDeInicio/PantallaDeInicio.jsx
+++ jpld_react/src/components/Pages/PantallaDeInicio/PantallaDeInicio.jsx
@@ -1,22 +1,21 @@
-import PropTypes from "prop-types";
 import React from "react";
-import {Iniciar} from '../../Buttons/Iniciar/Iniciar.jsx';
-import {Salir} from '../../Buttons/Salir/Salir.jsx';
+import PropTypes from "prop-types";
+import { Iniciar } from "../../Buttons/Iniciar/Iniciar.jsx";
+import { Salir } from "../../Buttons/Salir/Salir.jsx";
 import "./style.css";
 
-export const PantallaDeInicio = ({
-  IMG = "IMG-0841-1.png",
-  img = "IMG-0841-2.png",
-  IMG1 = "IMG-0846-1.png",
-  vector = "vector.svg",
-}) => {
+// Import images from assets
+import IMG from "../../../assets/IMG_0841_1.png";
+import img from "../../../assets/IMG_0841_2.png";
+import IMG1 from "../../../assets/IMG_0846_1.png";
+import vector from "../../../assets/gear.png";
+
+export const PantallaDeInicio = () => {
   return (
     <div className="pantalla-de-inicio">
       <div className="overlap-group">
         <img className="IMG" alt="Img" src={IMG} />
-
         <img className="img" alt="Img" src={img} />
-
         <Salir className="salir-instance" />
         <Iniciar className="iniciar-instance" />
         <img className="IMG-2" alt="Img" src={IMG1} />
@@ -27,11 +26,4 @@ export const PantallaDeInicio = ({
       </div>
     </div>
   );
-};
-
-PantallaDeInicio.propTypes = {
-  IMG: PropTypes.string,
-  img: PropTypes.string,
-  IMG1: PropTypes.string,
-  vector: PropTypes.string,
 };
